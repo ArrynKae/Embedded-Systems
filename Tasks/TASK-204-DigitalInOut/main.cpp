967223925a2d6ba4cb579d7700331354a576eb6b
ArrynKae <59576221+ArrynKae@users.noreply.github.com>
2022-09-30 16:18:14 +0100

    Revision

--- Tasks/TASK-204-DigitalInOut/main.cpp
+++ Tasks/TASK-204-DigitalInOut/main.cpp
@@ -13,14 +13,19 @@
 
 //BusInOut Traffic_Lights_2(PC_7,PC_8,PC_9);
 DigitalInOut grnLED(TRAF_GRN2_PIN, PinDirection::PIN_OUTPUT, PinMode::OpenDrainNoPull, 0);
+// Add another LED which flashes in the state opposite to the green led.
+// Create a DigitalInOut object with the red led pin label, in output mode, and the configuration is open-drain with no internal pull-up or pull-down resistor
+DigitalInOut redLED(TRAF_RED2_PIN, PinDirection::PIN_OUTPUT, PinMode::OpenDrainNoPull, 0);
 
 int main()
 {
     //Note the logic
     while (true) {
         grnLED = 1;
+        redLED = 0;     // opposite state to green led
         wait_us(1000000);
         grnLED = 0;
+        redLED = 1;     // opposite state to green led
         wait_us(1000000);
     }
 }
--- Tasks/Task-205-DigitalIn/main.cpp
+++ Tasks/Task-205-DigitalIn/main.cpp
@@ -12,16 +12,17 @@
 
 #define BTN1_PIN PG_0 
 #define BTN2_PIN PG_1
-#define BTN3_PIN PG_2 // No pull down
-#define BTN4_PIN PG_3 // No pull down
+#define BTN3_PIN PG_2 // No pull down -- this button will have to be handled differently
+#define BTN4_PIN PG_3 // No pull down -- this button will have to be handled differently
 
 // Inputs
 DigitalIn SW2(BTN1_PIN);
 DigitalIn SW3(BTN2_PIN);
-DigitalInOut SW4(BTN3_PIN,PIN_INPUT,PullDown,0);
-DigitalInOut SW5(BTN4_PIN,PIN_INPUT,PullDown,0);
+DigitalInOut SW4(BTN3_PIN,PIN_INPUT,PullDown,0);        // use DigitalInOut to configure the switches to use the internal pull-down resistors and there are no physical resistors on the module support board
+DigitalInOut SW5(BTN4_PIN,PIN_INPUT,PullDown,0);        // use DigitalInOut to configure the switches to use the internal pull-down resistors and there are no physical resistors on the module support board
 
 // Outputs
+// Configure the push-pull LEDs as outputs.
 DigitalOut ledRed(TRAF_RED1_PIN);
 DigitalOut ledYel(TRAF_YEL1_PIN);
 DigitalOut ledGrn(TRAF_GRN1_PIN);
@@ -39,6 +40,10 @@ int main()
             ledYel = 1;
         } else {
             ledYel = 0;
+        }
+
+        if (SW4.read() == 1) {
+            ledGrn
         }
     }
 }
